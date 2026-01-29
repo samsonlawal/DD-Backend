c4ba5334d384e90f55cfff82b6783ee5909d4f82
deji <lawal4211@gmail.com>
2026-01-29 01:42:07 +0100

    created admin user

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -53,7 +54,6 @@ const userSchema = new mongoose.Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 // Compare password
